bf03e1f38986757752eef9a537fcfcf1a5143c70
raziel7893 <raziel7893@gmail.com>
2024-08-13 12:29:05 +0200

    update STDT to using the steamcmd template, add A2s to make server info Query work change folder for userdata to the windowsgsm structure so backups will include it general cleanup

--- WindowsGSM/GameServer/SDTD.cs
+++ WindowsGSM/GameServer/SDTD.cs
@@ -3,6 +3,9 @@ using System.Diagnostics;
 using System.IO;
 using System;
 using WindowsGSM.GameServer.Query;
+using System.Threading;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Engine;
 
 namespace WindowsGSM.GameServer
 {
@@ -26,15 +29,11 @@ namespace WindowsGSM.GameServer
     /// The second one is used.
     /// 
     /// </summary>
-    class SDTD
+    class SDTD : SteamCMDAgent
     {
-        private readonly Functions.ServerConfig _serverData;
-
-        public string Error;
-        public string Notice;
-
         public const string FullName = "7 Days to Die Dedicated Server";
-        public string StartPath = "7DaysToDieServer.exe";
+        public override string StartPath => "7DaysToDieServer.exe";
+        public override bool loginAnonymous => true;
         public bool AllowsEmbedConsole = true;
         public int PortIncrements = 1;
         public dynamic QueryMethod = new A2S();
@@ -45,37 +44,50 @@ namespace WindowsGSM.GameServer
         public string Maxplayers = "8";
         public string Additional = string.Empty;
 
-        public string AppId = "294420";
+        public override string AppId => "294420";
 
-        public SDTD(Functions.ServerConfig serverData)
-        {
-            _serverData = serverData;
-        }
+        public string OldProfile = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "7DaysToDie\\Saves");
 
-        public async void CreateServerCFG()
+        public SDTD(ServerConfig serverData) : base(serverData) => base.serverData = serverData;
+
+        public void CreateServerCFG()
         {
             //Download serverconfig.xml
-            string configPath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, "serverconfig.xml");
-            if (await Functions.Github.DownloadGameServerConfig(configPath, FullName))
+            string configPath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, "serverconfig.xml");
+            string configTmp = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, "serverconfig.xml.template");
+
+            if (Functions.Github.DownloadGameServerConfig(configTmp, FullName).Result)
             {
-                string configText = File.ReadAllText(configPath);
-                configText = configText.Replace("{{hostname}}", _serverData.ServerName);
-                configText = configText.Replace("{{rcon_password}}", _serverData.GetRCONPassword());
-                configText = configText.Replace("{{port}}", _serverData.ServerPort);
-                configText = configText.Replace("{{telnetPort}}", (int.Parse(_serverData.ServerPort) - int.Parse(Port) + 8081).ToString());
+                //make a backup of the provided file
+                File.Move(configPath, configPath + ".bak");
+
+                //it would be possible to automatically move it, but that gets messy when the user has multiple servers
+                if (Directory.Exists(OldProfile))
+                    UI.CreateYesNoPromptV1("Old server userdata found", $"An Old Savegame found in {OldProfile}\n\n" +
+                        $"If you want to use it, you need to move/copy it manually to \n" +
+                        $"servers/{serverData.ServerID}/serverfiles/userdata", "Ok", "Ok");
+                string configText = File.ReadAllText(configTmp);
+                configText = configText.Replace("{{hostname}}", serverData.ServerName);
+                configText = configText.Replace("{{rcon_password}}", serverData.GetRCONPassword());
+                configText = configText.Replace("{{port}}", serverData.ServerPort);
+                configText = configText.Replace("{{telnetPort}}", (int.Parse(serverData.ServerPort) - int.Parse(Port) + 8081).ToString());
                 configText = configText.Replace("{{maxplayers}}", Maxplayers);
+
+                //it is not good to have the serverdata in %AppData% as the user will forget it, as nearly all windowsgsm servers store it inside the WindosGSM structure
+                //also the backup function would be useless without
+                configText = configText.Replace("<!-- <property name=\"UserDataFolder\"\t\t\t\tvalue=\"absolute path\" /> -->", "<property name=\"UserDataFolder\"\t\t\t\tvalue=\"userdata\" />");
                 File.WriteAllText(configPath, configText);
             }
 
             //Create steam_appid.txt
-            string txtPath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, "steam_appid.txt");
+            string txtPath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, "steam_appid.txt");
             File.WriteAllText(txtPath, "251570");
         }
 
         public async Task<Process> Start()
         {
             string exeName = "7DaysToDieServer.exe";
-            string workingDir = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID);
+            string workingDir = Functions.ServerPath.GetServersServerFiles(serverData.ServerID);
             string exePath = Path.Combine(workingDir, exeName);
 
             if (!File.Exists(exePath))
@@ -84,20 +96,25 @@ namespace WindowsGSM.GameServer
                 return null;
             }
 
-            string configPath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, "serverconfig.xml");
-            if (!File.Exists(configPath))
+            string configPath = Functions.ServerPath.GetServersServerFiles(serverData.ServerID, "serverconfig.xml");
+            if (!File.Exists(configPath) && new FileInfo(configPath).Length == 0)
             {
-                Notice = $"serverconfig.xml not found ({configPath})";
+                Notice = $"serverconfig.xml not found ({configPath}), reloading it from https://github.com/WindowsGSM/Game-Server-Configs";
+                CreateServerCFG();
+                if (!File.Exists(configPath))
+                    Error = $"ConfigFile is still missing {configPath}";
+            }
+
+            if (Directory.Exists(OldProfile))
+            {
+                Notice = Notice + $"An Old Savegame found in {OldProfile}\n\n" +
+                        $"If you want to use it, you need to move/copy it manually to \n" +
+                        $"servers/{serverData.ServerID}/serverfiles/userdata.\n" +
+                        $"If this is intentional, ignore this";
             }
 
             string logFile = @"7DaysToDieServer_Data\output_log_dedi__" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".txt";
-            string param = $"-logfile \"{Path.Combine(workingDir, logFile)}\" -quit -batchmode -nographics -configfile=serverconfig.xml -dedicated {_serverData.ServerParam}";
-
-            WindowsFirewall firewall = new WindowsFirewall(exeName, exePath);
-            if (!await firewall.IsRuleExist())
-            {
-                await firewall.AddRule();
-            }
+            string param = $"-logfile \"{Path.Combine(workingDir, logFile)}\" -quit -batchmode -nographics -configfile=serverconfig.xml -dedicated {serverData.ServerParam}";
 
             Process p;
             if (!AllowsEmbedConsole)
@@ -131,70 +148,32 @@ namespace WindowsGSM.GameServer
                     },
                     EnableRaisingEvents = true
                 };
-                var serverConsole = new Functions.ServerConsole(_serverData.ServerID);
+                var serverConsole = new Functions.ServerConsole(serverData.ServerID);
                 p.OutputDataReceived += serverConsole.AddOutput;
                 p.ErrorDataReceived += serverConsole.AddOutput;
                 p.Start();
                 p.BeginOutputReadLine();
                 p.BeginErrorReadLine();
             }
+            Console.WriteLine($"starting server:");
 
             return p;
         }
 
         public async Task Stop(Process p)
         {
-            await Task.Run(() =>
+            await Task.Run(async () =>
             {
                 Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
-                p.CloseMainWindow();
+                Functions.ServerConsole.SendWaitToMainWindow("^c");
+                Thread.Sleep(500);
                 Functions.ServerConsole.SendWaitToMainWindow("{ENTER}");
+
+                p.CloseMainWindow();
+                Thread.Sleep(1000);
+                if (!p.HasExited)
+                    p.Kill();
             });
-        }
-
-        public async Task<Process> Install()
-        {
-            var steamCMD = new Installer.SteamCMD();
-            Process p = await steamCMD.Install(_serverData.ServerID, string.Empty, AppId);
-            Error = steamCMD.Error;
-
-            return p;
-        }
-
-        public async Task<Process> Update(bool validate = false, string custom = null)
-        {
-            var (p, error) = await Installer.SteamCMD.UpdateEx(_serverData.ServerID, AppId, validate, custom: custom);
-            Error = error;
-            return p;
-        }
-
-        public bool IsInstallValid()
-        {
-            string exeFile = "7DaysToDieServer.exe";
-            string exePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, exeFile);
-
-            return File.Exists(exePath);
-        }
-
-        public bool IsImportValid(string path)
-        {
-            string exeFile = "7DaysToDieServer.exe";
-            string exePath = Path.Combine(path, exeFile);
-
-            Error = $"Invalid Path! Fail to find {exeFile}";
-            return File.Exists(exePath);
-        }
-
-        public string GetLocalBuild()
-        {
-            var steamCMD = new Installer.SteamCMD();
-            return steamCMD.GetLocalBuild(_serverData.ServerID, AppId);
-        }
-
-        public async Task<string> GetRemoteBuild()
-        {
-            var steamCMD = new Installer.SteamCMD();
-            return await steamCMD.GetRemoteBuild(AppId);
         }
     }
 }
